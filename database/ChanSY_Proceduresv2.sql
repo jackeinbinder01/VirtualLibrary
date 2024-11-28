cdf72997fe2e6608a9d96e53aebb6345bf34fd2e
sychan91 <c.saiyin@gmail.com>
2024-11-28 12:36:04 -0500

    removed redundant procedures
    removes redundant procedures

--- database/ChanSY_Proceduresv2.sql
+++ database/ChanSY_Proceduresv2.sql
@@ -17,62 +17,6 @@ Test cases:
 */
 CALL return_list_name_of_user('bob');
 
-Error Code: 1304. PROCEDURE return_list_name_of_user already exists
-
-
-/*
-Procedure to view booklist items. Takes in username and name of booklist.
-*/
-
-DELIMITER $$
-CREATE PROCEDURE return_book_list_items(user_name_p VARCHAR(64), book_list_name_p VARCHAR(64))
-BEGIN
-	SELECT b.book_id, b.book_title, b.description, b.release_date
-    FROM user_book_list ubl
-		JOIN book_list bl ON ubl.book_list_name = bl.list_name
-        JOIN book_list_book blb ON blb.book_list_name = bl.list_name
-        JOIN book b USING(book_id)
-	WHERE ubl.user_name = user_name_p AND ubl.book_list_name = book_list_name_p;
-END $$
-DELIMITER ;
-
--- Test cases:
-CALL return_book_list_items('jack', 'favorites');
-
-/*
-Second pass at retrieving books from a specific book list
-*/
-DELIMITER $$
-
-CREATE PROCEDURE fetch_books_in_list(
-    IN book_list_name_p VARCHAR(64)
-)
-BEGIN
-    SELECT 
-        b.book_id,
-        b.book_title,
-        b.release_date,
-        GROUP_CONCAT(DISTINCT g.genre_name) AS genreName,
-        GROUP_CONCAT(DISTINCT a.author_name) AS authorName,
-        GROUP_CONCAT(DISTINCT p.publisher_name) AS publisherName, 
-        GROUP_CONCAT(DISTINCT s.series_name) AS seriesName       
-    FROM book_list_book blb
-    JOIN book b ON blb.book_id = b.book_id
-    LEFT JOIN book_genre bg ON b.book_id = bg.book_id
-    LEFT JOIN genre g ON bg.genre_name = g.genre_name 
-    LEFT JOIN book_author ba ON b.book_id = ba.book_id
-    LEFT JOIN author a ON ba.author_id = a.author_id
-    LEFT JOIN book_publisher bp ON b.book_id = bp.book_id
-    LEFT JOIN publisher p ON bp.publisher_id = p.publisher_id
-    LEFT JOIN book_series bs ON b.book_id = bs.book_id
-    LEFT JOIN series s ON bs.series_id = s.series_id
-    WHERE blb.book_list_name = book_list_name_p
-    GROUP BY b.book_id, b.book_title, b.release_date
-    ORDER BY b.book_title;
-END $$
-
-DELIMITER ;
-
 /*
 Third pass at procedure for retrieving books from a specific list with new DB design
 */
@@ -107,13 +51,10 @@ END $$
 
 DELIMITER ;
         
-
 SELECT @@sql_mode;
 
 -- Test case:
 CALL fetch_books_in_list('test_list');
-
-CALL fetch_books_in_list("da vanci");
 
 /*
 Procedure that creates a new user book list
