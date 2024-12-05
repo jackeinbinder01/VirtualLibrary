464ed84372703a3b56dbb5cf328d65781e5ad80e
Jack Einbinder <jackeinbinder01@gmail.com>
2024-12-05 18:30:10 -0500

    loose ends

---
+++ VLapp/archive/controller_archive.py
@@ -0,0 +1,31 @@
+import model_archive as model
+import pymysql
+import db_and_app_login
+
+
+def work():
+    connection = model.connect_to_database()
+    if connection == None:
+        print("closing app")
+        return
+    try:
+        while True:
+            username = model.login_options(connection)
+            if username == False:
+                print("Failed to login to the Virtual Library. Closing application.")
+                return
+            else:
+                break
+    except Exception as e:
+        print(f"Errors occurred: {e}:")
+        return
+    model.application_logic(connection, username)
+    connection.commit()
+
+
+def main():
+    work()
+
+
+if __name__ == "__main__":
+    main()
