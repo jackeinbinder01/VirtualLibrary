611d2f458882ab78ab93e6a35c10177c509959c0
Jack Einbinder <jackeinbinder01@gmail.com>
2024-12-05 17:07:12 -0500

    loose ends

--- VLapp/model.py
+++ VLapp/model.py
@@ -231,7 +231,7 @@ def admin_main_menu():
     return answer
 
 
-def manage_users_menu(connection, username):
+def manage_users_menu(connection, admin_user_name):
     print("\nWelcome to the Manage Users Menu!\n"
           "Please select from the following options:\n")
     answer = input("1. View users in database"
@@ -243,23 +243,23 @@ def manage_users_menu(connection, username):
                    "\nr. Return to main menu\n\n")
     match answer.lower():
         case '1':
-            view_users(connection)
+            admin_view_users(connection, admin_user_name)
         case '2':
-            admin_create_user(connection)
+            admin_create_user(connection, admin_user_name)
         case '3':
-            admin_delete_user(connection)
+            admin_delete_user(connection, admin_user_name)
         case '4':
-            admin_update_user_information(connection)
+            admin_update_user_information(connection, admin_user_name)
         case '5':
-            make_user_admin(connection)
+            make_user_admin(connection, admin_user_name)
         case '6':
-            demote_user_from_admin(connection)
+            demote_user_from_admin(connection, admin_user_name)
         case 'r':
-            application_logic(connection, username)
+            application_logic(connection, admin_user_name)
 
 
 
-def view_users(connection):
+def admin_view_users(connection, admin_user_name):
     try:
         cursor = connection.cursor()
         cursor.execute("CALL view_users()")
@@ -271,24 +271,24 @@ def view_users(connection):
                 print("View users error: No users found in database.")
             else:
                 clean_data = [
-                    {key: ('True' if key == 'is_admin' and value == 1 else value) for key, value in row.items()}
+                    {key: ('True' if key == 'is_admin' and value == 1 else 'False') for key, value in row.items()}
                     for row in result_tuples
                 ]
                 table = tabulate.tabulate(clean_data, headers="keys", tablefmt="grid")
                 print(f'{table}')
-                manage_users_menu(connection)
+                manage_users_menu(connection, admin_user_name)
     except pymysql.Error as e:
         code, msg = e.args
         print(f"View users error: {code} - {msg}")
 
 
-def admin_create_user(connection):
+def admin_create_user(connection, admin_user_name):
     username = input("Enter the user's username: ").strip()
     password = input("Enter the user's password: ").strip()
 
     if username == '' or password == '':
         print("\nCreate user error: Username and/or password cannot be blank.")
-        manage_users_menu(connection)
+        manage_users_menu(connection, admin_user_name)
         return
 
     try:
@@ -299,12 +299,12 @@ def admin_create_user(connection):
         print(f"\nAdmin create user error: {e}\n")
 
 
-def admin_delete_user(connection):
+def admin_delete_user(connection, admin_user_name):
     username = input("Enter the username of the user to delete: ").strip()
 
     if username == '':
         print("\nDelete User Error: Username cannot be blank.")
-        manage_users_menu(connection)
+        manage_users_menu(connection, admin_user_name)
         return
 
     try:
@@ -315,7 +315,7 @@ def admin_delete_user(connection):
         print(f"\nDelete user error: {e}\n")
 
 
-def admin_update_user_information(connection):
+def admin_update_user_information(connection, admin_user_name):
     print("Please select from the following options:\n")
     answer = input("\n1. Update a user's username"
                    "\n2. Update a user's password"
@@ -329,11 +329,11 @@ def admin_update_user_information(connection):
 
             if old_username == new_username:
                 print("\nUpdate user error: New username must be different than the original username.")
-                admin_update_user_information(connection)
+                admin_update_user_information(connection, admin_user_name)
                 return
             if new_username == '':
                 print("\nUpdate user error: New username cannot be blank.")
-                admin_update_user_information(connection)
+                admin_update_user_information(connection, admin_user_name)
                 return
 
             try:
@@ -348,7 +348,7 @@ def admin_update_user_information(connection):
 
             if new_password == '':
                 print("\nUpdate user error: New password cannot be blank.")
-                admin_update_user_information(connection)
+                admin_update_user_information(connection, admin_user_name)
                 return
 
             try:
@@ -365,15 +365,15 @@ def admin_update_user_information(connection):
 
             if old_username == new_username:
                 print("\nUpdate user error: New username must be different than the original username.")
-                admin_update_user_information(connection)
+                admin_update_user_information(connection, admin_user_name)
                 return
             if new_username == '':
                 print("\nUpdate user error: New username cannot be blank.")
-                admin_update_user_information(connection)
+                admin_update_user_information(connection, admin_user_name)
                 return
             if new_password == '':
                 print("\nUpdate user error: New password cannot be blank.")
-                admin_update_user_information(connection)
+                admin_update_user_information(connection, admin_user_name)
                 return
             try:
                 cursor = connection.cursor()
@@ -389,24 +389,24 @@ def admin_update_user_information(connection):
             except pymysql.Error as e:
                 print(f"\nUpdate user error: {e}\n")
         case 'r':
-            manage_users_menu(connection)
+            manage_users_menu(connection, admin_user_name)
         case _:
             print(f"\nInvalid option '{answer}'. Please try again.")
-            admin_update_user_information(connection)
+            admin_update_user_information(connection, admin_user_name)
 
 
-def make_user_admin(connection):
+def make_user_admin(connection, admin_user_name):
     username = input("Enter the user's username: ").strip()
 
     if username == '':
         print("\nMake user admin error: Username cannot be blank.")
-        manage_users_menu(connection)
+        manage_users_menu(connection, admin_user_name)
         return
 
     confirmation = input(f"Are you sure you want to make '{username}' an Admin? (y/n)\n ")
     if confirmation.lower() != 'y':
         print(f"\nUser '{username}' was NOT made an Admin.")
-        manage_users_menu(connection)
+        manage_users_menu(connection, admin_user_name)
         return
 
     try:
@@ -422,13 +422,13 @@ def demote_user_from_admin(connection):
 
     if username == '':
         print("\nDemote user from admin error: Username cannot be blank.")
-        manage_users_menu(connection)
+        manage_users_menu(connection, admin_user_name)
         return
 
     confirmation = input(f"Are you sure you want to demote '{username}' from Admin? (y/n)\n ")
     if confirmation.lower() != 'y':
         print(f"\nUser '{username}' was NOT demoted from Admin.")
-        manage_users_menu(connection)
+        manage_users_menu(connection, admin_user_name)
         return
 
     try:
@@ -449,7 +449,6 @@ def search_menu(current_list=None):
                    "\n5. Add a URL to access a copy of a book"
                    "\n\nr. Return to the main menu\n\n")
     return answer
-
 
 
 def manage_menu(username):
@@ -511,7 +510,7 @@ def application_logic(connection, username):
             analysis_logic(connection, username)
 
         elif main_menu_answer.strip() == "4" and user_is_admin:
-            manage_users_menu(connection)
+            manage_users_menu(connection, username)
 
 
 
